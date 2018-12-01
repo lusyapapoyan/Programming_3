24ff34e3267bb0098f7e1e039d6f6c365988fd1e
Your Name <you@example.com>
2018-12-01 12:34:08 +0400

    add all files

---
+++ public/class.owner.js
@@ -0,0 +1,84 @@
+
+class Owner {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.directions = [];
+        this.acted = false;
+        this.keracKarmirQanak = 0;
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+
+            [this.x - 2, this.y - 1],
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x + 2, this.y - 1],
+
+            [this.x - 2, this.y],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x + 2, this.y],
+
+            [this.x - 2, this.y + 1],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+            [this.x + 2, this.y + 1],
+
+            [this.x - 2, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x + 2, this.y + 2]
+
+        ];
+    }
+    chooseCell(num) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push([x, y]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push([x, y]);
+                }
+            }
+        }
+        return found;
+    }
+
+    shoot() {
+        var pred = this.chooseCell(3);
+        for (var i in pred) {
+            this.keracKarmirQanak++;
+            var x = pred[i][0];
+            var y = pred[i][1];
+            matrix[y][x].die();
+
+            if (this.keracKarmirQanak >= 10) {
+                this.die();
+
+            }
+
+        }
+
+
+    }
+    die() {
+        matrix[this.y][this.x] = 0;
+    }
+
+}
---
+++ public/class.water.js
@@ -0,0 +1,62 @@
+
+class Water {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.directions = [];
+        this.acted = false;
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+
+
+        ];
+    }
+
+    chooseCell(num) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push([x, y]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push([x, y]);
+                }
+            }
+        }
+        return found;
+    }
+
+    adden() {
+        var yellow = this.chooseCell(2);
+        for (var i in yellow) {
+            var x = yellow[i][0];
+            var y = yellow[i][1];
+            matrix[y][x].energy += 5;
+
+        }
+        var red = this.chooseCell(3);
+        for (var i in red) {
+            var x = red[i][0];
+            var y = red[i][1];
+            matrix[y][x].energy += 8;
+
+
+        }
+
+    }
+
+}
