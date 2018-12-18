8a832a31f296930bc0900e3a63cd8bc274968a04
Your Name <you@example.com>
2018-12-18 17:05:38 +0400

    changes

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'modules')));
 
 // Listen for requests
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function () {
   var port = server.address().port;
   console.log('Magic happens on port ' + port);
 });
 
 var matrix = require("./modules/matrix.js");
 console.log(matrix);
+
+
+io.on("connection", function (socket) {
+  socket.emit("matrix", messages[i]);
+
+});
--- modules/matrix.js
+++ modules/matrix.js
@@ -1,9 +1,9 @@
 var LivingCreature = require("./class.LivingCreature");
-var Grass =require("./class.grass");
-var GrassEater =require("./class.grasseater");
-var Owner =require("./class.owner");
-var Gishatich =require("./class.gishatich");
-var Water =require("./class.water");
+var Grass = require("./class.grass");
+var GrassEater = require("./class.grasseater");
+var Owner = require("./class.owner");
+var Gishatich = require("./class.gishatich");
+var Water = require("./class.water");
 
 var matrix = [];
 var n = 80;
@@ -13,40 +13,31 @@ var side = 10;
 for (var y = 0; y < n; y++) {
     matrix[y] = [];
     for (var x = 0; x < m; x++) {
-        matrix[y][x] =  function getRandomInt(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;  
-          }
-    
-          console.log(getRandomInt(0,5));
-        
+        matrix[y][x] = Math.floor((Math.random() * 5) + 0);
 
     }
 
 }
-    for (var y = 0; y < matrix.length; y++) {
-        for (var x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] == 1) {
-                matrix[y][x] = new Grass(x, y, 1);
-            }
-            else if (matrix[y][x] == 2) {
-                matrix[y][x] = new GrassEater(x, y, 2);
-            }
-            else if (matrix[y][x] == 3) {
-                matrix[y][x] = new Gishatich(x, y, 3);
-            }
-            else if (matrix[y][x] == 4) {
-                matrix[y][x] = new Water(x, y, 4);
-            }
-            else if (matrix[y][x] == 5) {
-                matrix[y][x] = new Owner(x, y, 5);
-            }
+
+for (var y = 0; y < matrix.length; y++) {
+    for (var x = 0; x < matrix[y].length; x++) {
+        if (matrix[y][x] == 1) {
+            matrix[y][x] = new Grass(x, y, 1);
+        }
+        else if (matrix[y][x] == 2) {
+            matrix[y][x] = new GrassEater(x, y, 2);
+        }
+        else if (matrix[y][x] == 3) {
+            matrix[y][x] = new Gishatich(x, y, 3);
+        }
+        else if (matrix[y][x] == 4) {
+            matrix[y][x] = new Water(x, y, 4);
+        }
+        else if (matrix[y][x] == 5) {
+            matrix[y][x] = new Owner(x, y, 5);
         }
     }
-   
+}
 
+module.exports = matrix;
 
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;  
-      }
-
-      console.log(getRandomInt(0,5));
